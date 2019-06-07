728cd1374e3a0478799f9bed55c22478ce313aa5
Melanie Schicker <melanie.schicker@gmail.com>
2019-06-07 16:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ function whisper(string) {
 }
 
 function logShout(string) {
+<<<<<<< HEAD
   console.log(string.toUpperCase());
+=======
+  cosole.log(string.toUpperCase());
+>>>>>>> 56440e8615d5b3dd112c45964a720a895017f47b
 }
 
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
+<<<<<<< HEAD
 function sayHiToGrandma(string) {
   if (string == string.toLowerCase()) 
     return "I can't hear you!";
@@ -21,4 +26,8 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   else if (string == "I love you, Grandma.")
     return "I love you, too.";
+=======
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+>>>>>>> 56440e8615d5b3dd112c45964a720a895017f47b
 }
